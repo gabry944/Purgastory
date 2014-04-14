b13a15a550324bf5147ca448f64bb557787fae25
unknown <gabry944@student.liu.se>
2014-04-14 11:31:39 +0200

    sliderfunktion som har en global variabel

---
+++ Assets/slider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slider : MonoBehaviour {
+
+	public GUISkin skin;
+	static public int coins = 0;
+	private float sliderValue = 1.0f;
+
+	void OnGUI(){
+
+		GUI.skin = skin;
+		sliderValue = GUI.VerticalSlider(new Rect(Screen.width - 20, 10, 0, 100), sliderValue, 2.0f, 0.0f);	
+	}
+}
