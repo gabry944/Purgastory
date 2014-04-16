9ef2a2e30d3c11c1a456592071526a20a1637a95
Isafo <isabelle.forsman@gmail.com>
2014-04-16 10:49:17 +0200

    updating score when game finished

--- assets/slider.cs
+++ assets/slider.cs
@@ -5,13 +5,24 @@ public class slider : MonoBehaviour {
 
 
 	static public int coins = 0;
-
+	static public bool lvlFinished = false;
 	public GUISkin skin;
 	private float sliderValue = 1.0f;
+	static public bool gameStarted = false;
 
 	void OnGUI(){
+		if(gameStarted == true)
+			updateScore ();
 
 		GUI.skin = skin;
-		sliderValue = GUI.VerticalSlider(new Rect(Screen.width - 20, 10, 0, 100), sliderValue, 2.0f, 0.0f);	
+		sliderValue = GUI.VerticalSlider(new Rect(Screen.width - 20, 10, 0, 100), coins, 2.0f, 0.0f);	
+	}
+
+
+	void updateScore(){
+		if (lvlFinished == true)
+			sliderValue += 1;
+		else
+			sliderValue -= 1;
 	}
 }
