2e39bc405f7f97c6609a4d36da09cf95f341280d
alundin <alilu684@student.liu.se>
2014-04-17 10:58:26 +0200

    initial commit with portals

---
+++ Assets/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == 3rdPersonController )
+		{
+			DontDestroyOnLoad(other.gameObject);
+			Application.LoadLevel("Next Level");
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
