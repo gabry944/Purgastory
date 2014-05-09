5a3d4348e87a8196b3c546d0f0328ef9f9fe9ff7
Isafo <isabelle.forsman@gmail.com>
2014-05-09 09:19:34 +0200

    loading different textfiles for different senarios

--- assets/messageTrigger.cs
+++ assets/messageTrigger.cs
@@ -9,19 +9,32 @@ public class messageTrigger : MonoBehaviour {
 	private bool inside = false;
 	protected StreamReader reader = null;
 	protected string line = "";
-	private bool fileNotRead = false;
+	private bool fileNotRead = true;
 	private string text = "";
-
+	private int EOF = 1;
 
 	void OnTriggerEnter(Collider c){
 		text = "Press e to interact";
 		OnGUI ();
 
-		if(fileNotRead == false){
-			reader = new StreamReader(@"angelConv.txt");
-			fileNotRead = true;
+		if(fileNotRead == true){
+			loadFile();
 		}
+
 		inside = true;
+	}
+
+	void loadFile(){
+		switch(EOF){
+		case 1:
+			reader = new StreamReader(@"angelConv.txt");
+			fileNotRead = false;
+			break;
+		case 2:
+			reader = new StreamReader(@"labWin.txt");
+			fileNotRead = false;
+			break;
+		}
 	}
 
 	void OnTriggerExit(Collider c){
@@ -30,11 +43,15 @@ public class messageTrigger : MonoBehaviour {
 	}
 
 	void Update(){
-
 		if (inside == true && Input.GetKeyDown("e")){
 			line = reader.ReadLine();
 			if(line != null){
 				text = line;
+			}
+			else{
+				fileNotRead = true;
+				EOF++;
+				loadFile ();
 			}
 			OnGUI ();
 		}
