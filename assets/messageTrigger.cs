13a67e0eb6cc6776406ac2135259885422382179
Isafo <isabelle.forsman@gmail.com>
2014-05-09 10:09:28 +0200

    Interaction alternetives for beggar added

--- assets/messageTrigger.cs
+++ assets/messageTrigger.cs
@@ -12,6 +12,7 @@ public class messageTrigger : MonoBehaviour {
 	private bool fileNotRead = true;
 	private string text = "";
 	private int EOF = 1;
+	public string[] answerButtons;
 
 	void OnTriggerEnter(Collider c){
 		text = "Press e to interact";
@@ -61,6 +62,21 @@ public class messageTrigger : MonoBehaviour {
 		if (inside == true)
 		{
 			GUI.Box(new Rect(900, 500, 250, 150), text);
+
+
+			if(EOF == 2){
+				if(GUI.Button(new Rect(1200,500,200,30), answerButtons[0])){
+
+				}
+
+				if(GUI.Button(new Rect(1200,535,200,30), answerButtons[1])){
+					
+				}
+				if(GUI.Button(new Rect(1200,570,200,30), answerButtons[2])){
+					
+				}
+
+			}
 		}
 	}
 }
